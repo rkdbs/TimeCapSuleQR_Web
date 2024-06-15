aca48ed18cdae859b45bff05eed3ef014c51e631
rkdbs <w2207@e-mirim.hs.kr>
2024-06-16 08:27:53 +0900

    Feat: img file upload

--- src/App.js
+++ src/App.js
@@ -1,19 +1,35 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import logo from './timecapsule.png';
 import gallery from './gallery.png';
 import './App.css';
 
 function App() {
+  const fileInputRef = useRef(null);
+
+  const uploadImg = () => {
+    fileInputRef.current.click();
+  }
+
   return (
     <div className="container">
       <img src={logo} />
 
-      <div className='box'> 
+      <div className='box' onClick={uploadImg} > 
         <div>
           <img src={gallery} />
           <p>이미지 업로드</p>
         </div>
       </div>
+
+      <input
+        type="file"
+        accept="image/*"
+        ref={fileInputRef}
+        style={{ display: 'none' }}
+        onChange={(event) => {
+          console.log(event.target.files[0]);
+        }}
+      />
 
       <div className='button'>
         이미지 적용
