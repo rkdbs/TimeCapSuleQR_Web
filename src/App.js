da327f2c8eacd014114d4a3085e8529a54c5d2a2
rkdbs <mw2207@naver.com>
2024-06-18 14:30:52 +0900

    Feat: 업로드 된 이미지 서버 연결

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { useRef } from 'react';
 import logo from './timecapsule.png';
 import gallery from './gallery.png';
 import './App.css';
+import axios from 'axios';
 
 function App() {
   const fileInputRef = useRef(null);
@@ -10,13 +11,41 @@ function App() {
     fileInputRef.current.click();
   }
 
+  const applyImg = async () => {
+    if (fileInputRef.current.files.length === 0) {
+      alert('이미지를 업로드 해주세요!');
+      return;
+    }
+
+    const file = fileInputRef.current.files[0];
+    const formData = new FormData();
+    formData.append('capsuleImage', file);
+
+    try {
+      const res = await axios.post(`${process.env.REACT_APP_HOST}/letters/capsule`, formData, {
+        headers: {
+          "Content-Type": "multipart/form-data"
+        }
+      });
+      if (res.status === 200) {
+        console.log("이미지 업로드 성공");
+      }
+    } catch (error) {
+      console.error("에러 발생", error);
+    }
+
+    for (const [key, value] of formData.entries()) {
+      console.log(key, value.name);
+    };
+  }
+
   return (
     <div className="container">
-      <img src={logo} />
+      <img src={logo} alt='' />
 
       <div className='box' onClick={uploadImg} > 
         <div>
-          <img src={gallery} />
+          <img src={gallery} alt='' />
           <p>이미지 업로드</p>
         </div>
       </div>
@@ -27,12 +56,11 @@ function App() {
         ref={fileInputRef}
         style={{ display: 'none' }}
         onChange={(event) => {
-          console.log(event.target.files[0]);
-          alert("이미지 업로드가 완료되었습니다");
+          alert("이미지 업로드가 완료되었습니다. 이미지 적용 버튼을 눌러주세요!");
         }}
       />
 
-      <div className='button'>
+      <div className='button' onClick={applyImg}>
         이미지 적용
       </div>
 
