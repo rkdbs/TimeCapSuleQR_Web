e5195befa92f802d52690c872b9b423c9fbe966e
rlagkdms <ithaeun@naver.com>
2024-06-21 08:59:22 +0900

    refector : put 으로 수정

--- src/App.js
+++ src/App.js
@@ -1,3 +1,87 @@
+// import React, { useRef, useState } from "react";
+// import logo from "./timecapsule.png";
+// import gallery from "./gallery.png";
+// // import random from "./random.png";
+// import "./App.css";
+// import axios from "axios";
+
+// function App() {
+//   const fileInputRef = useRef(null);
+//   const [hasClicked, setHasClicked] = useState(false);
+//   const [selectedImage, setSelectedImage] = useState(null);
+
+//   const uploadImg = () => {
+//     fileInputRef.current.click();
+//     setHasClicked(true);
+//     setSelectedImage(null);
+//   };
+
+//   const applyImg = async () => {
+//     if (!hasClicked) {
+//       alert("이미지를 업로드해주세요!");
+//       return;
+//     }
+//     console.log(selectedImage);
+
+//     const formData = new FormData();
+
+//     if (selectedImage) {
+//       formData.append("imageUrl", selectedImage);
+//     } else {
+//       const file = fileInputRef.current.files[0];
+//       formData.append("capsuleImage", file);
+//     }
+
+//     try {
+//       const res = await axios.post(
+//         `${process.env.REACT_APP_HOST}/letters/capsule`,
+//         formData,
+//         {
+//           headers: {
+//             "Content-Type": "multipart/form-data",
+//           },
+//         }
+//       );
+//       if (res.status === 200) {
+//         console.log("이미지 업로드 성공");
+//       }
+//     } catch (error) {
+//       console.error("에러 발생", error);
+//     }
+//   };
+
+//   return (
+//     <div className="container">
+//       <img src={logo} alt="" />
+
+//       <div className="box">
+//         <div onClick={uploadImg}>
+//           <img src={gallery} alt="" />
+//           <p>이미지 업로드</p>
+//         </div>
+//       </div>
+
+//       <input
+//         type="file"
+//         accept="image/jpeg"
+//         ref={fileInputRef}
+//         style={{ display: "none" }}
+//         onChange={(event) => {
+//           console.log(event.target.files[0]);
+//           setHasClicked(true);
+//           setSelectedImage(event.target.files[0]);
+//         }}
+//       />
+
+//       <div className="button" onClick={applyImg}>
+//         이미지 적용
+//       </div>
+//     </div>
+//   );
+// }
+
+// export default App;
+
 import React, { useRef, useState } from "react";
 import logo from "./timecapsule.png";
 import gallery from "./gallery.png";
@@ -13,23 +97,19 @@ function App() {
   const uploadImg = () => {
     fileInputRef.current.click();
     setHasClicked(true);
-    setSelectedImage(null);
   };
 
   const applyImg = async () => {
-    if (!hasClicked) {
+    if (!hasClicked || !selectedImage) {
       alert("이미지를 업로드해주세요!");
       return;
     }
 
+    console.log("Selected Image:", selectedImage);
+
     const formData = new FormData();
 
-    if (selectedImage) {
-      formData.append("imageUrl", selectedImage);
-    } else {
-      const file = fileInputRef.current.files[0];
-      formData.append("capsuleImage", file);
-    }
+    formData.append("capsuleImage", selectedImage);
 
     try {
       const res = await axios.post(
@@ -66,9 +146,9 @@ function App() {
         ref={fileInputRef}
         style={{ display: "none" }}
         onChange={(event) => {
-          console.log(event.target.files[0].name);
+          console.log("Selected file:", event.target.files[0]);
           setHasClicked(true);
-          setSelectedImage(null);
+          setSelectedImage(event.target.files[0]);
         }}
       />
 
